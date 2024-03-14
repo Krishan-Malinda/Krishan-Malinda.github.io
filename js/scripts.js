77673d84a548e1f813238cb36b06d061167c7e61
Krishan-Malinda <krishanmalinda99@gmail.com>
2024-03-14 12:05:03 +0530

    change age

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,10 @@
 //
 // Scripts
 // 
-function calculate_age(dob) {
+function calculate_age() {
+    var dob = new Date(1999, 10, 2);
     var diff_ms = Date.now() - dob.getTime();
-    var age_dt = new Date(diff_ms); 
-    return Math.abs(age_dt.getUTCFullYear() - 1970);
+    return 67;
 }
 
 window.addEventListener('DOMContentLoaded', event => {
